88ae907e5b4b68dc10860ffa06391a1cc06a8292
Steve Brouillard <sjbrouillard@gmail.com>
2024-11-12 09:36:24 -0500

    FEAT: Add initial attempt at home startup profile script

---
+++ Source/Start-HomeProfile.ps1
@@ -0,0 +1,38 @@
+Write-Host "Starting BEACN App..."
+Start-Process -FilePath "C:\Program Files\BEACN\BEACN App\BEACN.exe"
+
+Write-Host "Starting Display Pilot 2..."
+Start-Process -FilePath "C:\Program Files\BenQ\Display Pilot 2\Display Pilot 2.exe"
+
+Write-Host "Starting Elgato Control Center..."
+Start-Process -FilePath "C:\Program Files\Elgato\ControlCenter\ControlCenter.exe" -ArgumentList "-runinbk"
+
+Write-Host "Starting Elgato Stream Deck..."
+Start-Process -FilePath "C:\Program Files\Elgato\StreamDeck\StreamDeck.exe" -ArgumentList "-runinbk"
+
+Write-Host "Starting Adobe Updater Startup Utility..."
+Start-Process -FilePath "C:\Program Files (x86)\Common Files\Adobe\OOBE\PDApp\UWA\UpdaterStartupUtility.exe"
+
+Write-Host "Starting AdobeGCClient..."
+Start-Process -FilePath "C:\Program Files (x86)\Common Files\Adobe\AdobeGCClient\AGCInvokerUtility.exe"
+
+Write-Host "Starting Adobe Creative Cloud Experience..."
+Start-Process -FilePath "C:\Program Files (x86)\Adobe\Adobe Creative Cloud Experience\CCXProcess.exe"
+
+Write-Host "Starting Adobe Creative Cloud..."
+Start-Process -FilePath "C:\Program Files\Adobe\Adobe Creative Cloud\ACC\Creative Cloud.exe" -ArgumentList "--showwindow=false", "--onOSstartup=true"
+
+Write-Host "Starting WinZip Update Notifier..."
+Start-Process -FilePath "C:\Program Files\WinZip\WZUpdateNotifier.exe" -ArgumentList "-show"
+
+Write-Host "Starting Brother Software Update Notification Service..."
+Start-Process -FilePath "C:\Program Files (x86)\Brother\SoftwareUpdateNotification\SoftwareUpdateNotificationService.exe" -ArgumentList "/Autorun"
+
+Write-Host "Starting Brother Status Monitor..."
+Start-Process -FilePath "C:\Program Files (x86)\Browny02\Brother\BrStMonW.exe" -ArgumentList "/Autorun"
+
+Write-Host "Starting SpyderUtility..."
+Start-Process -FilePath "C:\Program Files (x86)\Datacolor\SpyderXElite\Utility\SpyderUtility.exe"
+
+Write-Host "Starting Dyn Updater..."
+Start-Process -FilePath "C:\Program Files (x86)\Dyn\Updater\dyn_updater.exe"
